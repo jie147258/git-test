1818b54331d2a9b72582084435c71600cdfb6df5
root <root@qq.com>
2022-06-29 20:50:19 +0800

    hot-fix3 commit2

--- src/main/java/com/bjpowernode/git/GitTest.java
+++ src/main/java/com/bjpowernode/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("这是一次分支的切换");
         System.out.println("准备开始合并");
         System.out.println("你可以在里面输入内容");
+        System.out.println("这是一个修改版本");
     }
 }
