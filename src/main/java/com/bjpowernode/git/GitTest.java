31b7c6211a69a4c559868d6edb63a79cfdb09deb
root <root@qq.com>
2022-06-29 18:55:24 +0800

    second commit

--- src/main/java/com/bjpowernode/git/GitTest.java
+++ src/main/java/com/bjpowernode/git/GitTest.java
@@ -3,5 +3,6 @@ package com.bjpowernode.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println( "学java来动力节点");
+        System.out.println("快来学java");
     }
 }
